5851cd90f7f0485125907a56c6c737fe1b353598
byio <ben@benyang.io>
2018-07-21 18:31:47 +0800

    install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
-import { Router, browserHistory, Route, Link } from 'react-router';
+import { Router, browserHistory, Route } from 'react-router';
+import { Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+const Page = ({ title }) => (
+  <div className="App">
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>{title}</h2>
+    </div>
+    <p className="App-intro">
+      This is the title page.
+    </p>
+    <p>
+      <Link to="/">Home</Link>
+    </p>
+    <p>
+      <Link to="/about">About</Link>
+    </p>
+    <p>
+      <Link to="/">Home</Link>
+    </p>
+  </div>
+);
 
 class App extends Component {
   render() {
