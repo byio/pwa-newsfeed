37159936d058458c35665c163ce6f8a3fb6d140c
byio <ben@benyang.io>
2018-08-01 17:40:26 +0800

    write test to check that CardList component render a Card component for each item in an array passed through the news prop

--- src/components/CardList/Cardlist.test.js
+++ src/components/CardList/Cardlist.test.js
@@ -9,11 +9,26 @@ Enzyme.configure({ adapter: new Adapter() });
 
 // Tests
 describe('CardList Component', () => {
-  let wrapper;
-  beforeEach(() => {
-    wrapper = shallow(<CardList />);
-  });
   it('should render without throwing an error', () => {
+    const wrapper = shallow(<CardList />);
     expect(wrapper.find('div.cardList').exists()).toBe(true);
+  });
+  it('should render n Card components for n news articles', () => {
+    const mockNewsArray = [
+      {
+        source: {
+          id: null,
+          name: 'Androidcentral.com'
+        },
+        author: 'Andrew Martonik',
+        title: 'title of article',
+        description: 'description of article',
+        url: 'https://www.androidcentral.com/oneplus-just-surpassed-samsungs-sales-india-first-time',
+        urlToImage: 'https://www.androidcentral.com/sites/androidcentral.com/files/styles/large/public/article_images/2018/05/oneplus-6-vs-galaxy-s9-plus-10.jpg?itok=9vBCskjM',
+        publishedAt: '2018-07-31T04:30:02Z'
+      }
+    ];
+    const wrapper = shallow(<CardList news={mockNewsArray} />);
+    expect(wrapper.find('Card').length).toBe(mockNewsArray.length);
   });
 });
