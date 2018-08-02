d00210510538300d1cea2aa6eaab9b75cbf51bf5
byio <ben@benyang.io>
2018-08-02 11:07:11 +0800

    move Page component into layouts folder; move Home and About component into pages folder; make Home component a stateful component

--- src/App.js
+++ src/App.js
@@ -2,30 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import './App.css';
-import CardList from './components/CardList/CardList';
-
-const Page = ({ title }) => (
-  <div className="App">
-    <ul className="nav">
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/about">About</Link></li>
-    </ul>
-  </div>
-);
-
-const Home = (props) => (
-  <div>
-    <Page title="Home" />
-    <CardList />
-  </div>
-);
-
-const About = (props) => (
-  <div>
-    <Page title="About" />
-    <p>This is a newsfeed app built using <a href="https://reactjs.org">ReactJS</a> and the <a href="https://newsapi.org">NewsAPI</a> based on Progressive Web App (PWA) specifications.</p>
-  </div>
-);
+import Home from './pages/Home';
+import About from './pages/About';
 
 class App extends Component {
   render() {
