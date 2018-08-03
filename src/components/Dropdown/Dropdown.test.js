899831ebf8e9563c7238ac9f6ba4418c02fddbae
byio <ben@benyang.io>
2018-08-03 09:26:30 +0800

    write failing tests for basic functionality of Dropdown component

--- src/components/Dropdown/Dropdown.test.js
+++ src/components/Dropdown/Dropdown.test.js
@@ -13,4 +13,30 @@ describe('Select Component', () => {
     const wrapper = shallow(<Dropdown />);
     expect(wrapper.find('div.dropdown').exists()).toBe(true);
   });
+  it('should have 1 select tag', () => {
+    const wrapper = shallow(<Dropdown />);
+    expect(wrapper.find('select').exists()).toBe(true);
+  });
+  it('should render at least 1 option tag', () => {
+    const wrapper = shallow(<Dropdown />);
+    expect(wrapper.find('option').length).toBe(1);
+  });
+  it('should render n+1 option tags for n sources', () => {
+    const mockSources = [
+      {
+        id: 'mock-source-1',
+        name: 'Mock Source 1'
+      },
+      {
+        id: 'mock-source-2',
+        name: 'Mock Source 2'
+      },
+      {
+        id: 'mock-source-3',
+        name: 'Mock Source 3'
+      }
+    ];
+    const wrapper = shallow(<Dropdown sources={mockSources} />);
+    expect(wrapper.find('option').length).toBe(mockSources.length + 1);
+  });
 });
